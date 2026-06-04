28def6a5983e4e68a0218558a83794a11c56b9bd
Aiden Thacker <aidenthacker86@gmail.com>
2026-06-03 19:31:20 -0500

    Simple Crouching is in

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,13 @@ public class PlayerMovement : MonoBehaviour
     public float deceleration = 10f;
 
     [SerializeField] private float currentMoveSpeed;
+    private float targetSpeed;
+
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+    private bool isCrouching;
 
     [Header("Jumping")]
     public float jumpForce;
@@ -34,13 +41,21 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Input")]
     [SerializeField] private InputActionAsset playerControls;
-
     private InputAction moveAction;
     private InputAction lookAction;
     private InputAction jumpAction;
     private InputAction sprintAction;
     private InputAction crouchAction;
     private Vector2 moveInput;
+
+    public enum MovementState
+    {
+        Walking,
+        Sprinting,
+        Air,
+        Crouching,
+    }
+    private MovementState state;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -54,6 +69,8 @@ public class PlayerMovement : MonoBehaviour
 
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+
+        startYScale = transform.localScale.y;
 
         readyToJump = true;
     }
@@ -84,20 +101,35 @@ public class PlayerMovement : MonoBehaviour
 
         isSprinting = sprintAction.ReadValue<float>() > 0;
 
-        float targetSpeed = moveSpeed * (isSprinting ? sprintMultiplier : 1f);
+        isCrouching = crouchAction.ReadValue<float>() > 0;
 
-        if (moveInput.magnitude > 0.1f)
+        if (isCrouching)
         {
-            currentMoveSpeed = Mathf.Lerp(currentMoveSpeed, targetSpeed, acceleration * Time.deltaTime);
+            targetSpeed = crouchSpeed;
+        }
+        else if (isSprinting)
+        {
+            targetSpeed = moveSpeed * sprintMultiplier;
         }
         else
         {
-            currentMoveSpeed = Mathf.Lerp(currentMoveSpeed, 0f, deceleration * Time.deltaTime);
+            targetSpeed = moveSpeed;
+        }
+
+        if (moveInput.magnitude > 0.1f)
+        {
+            currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, targetSpeed, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, 0f, deceleration * Time.deltaTime);
         }
 
         SpeedControl();
 
-        if(isGrounded && readyToJump && jumpAction.ReadValue<float>() > 0)
+        Crouch();
+
+        if (isGrounded && readyToJump && !isCrouching && jumpAction.ReadValue<float>() > 0)
         {
             readyToJump = false;
             Jump();
@@ -121,17 +153,15 @@ public class PlayerMovement : MonoBehaviour
 
     void HandleMovement()
     {
-        float speedMultipler = isSprinting ? sprintMultiplier : 1f;
-
         moveDirection = orientation.forward * moveInput.y + orientation.right * moveInput.x;
 
         if(isGrounded)
         {
-            rb.AddForce(moveDirection.normalized * currentMoveSpeed * speedMultipler * 10f, ForceMode.Force);
+            rb.AddForce(moveDirection.normalized * currentMoveSpeed * 10f, ForceMode.Force);
         }
         else if(!isGrounded)
         {
-            rb.AddForce(moveDirection.normalized * currentMoveSpeed * speedMultipler * airMultiplier * 10f, ForceMode.Force);
+            rb.AddForce(moveDirection.normalized * currentMoveSpeed * airMultiplier * 10f, ForceMode.Force);
         }
 
         //isMoving = moveInput.y != 0 || moveInput.x != 0;
@@ -139,11 +169,9 @@ public class PlayerMovement : MonoBehaviour
 
     void SpeedControl()
     {
-        float speedMultipler = isSprinting ? sprintMultiplier : 1f;
-
         Vector3 flatVal = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
 
-        float maxSpeed = currentMoveSpeed;
+        float maxSpeed = targetSpeed;
 
         if(flatVal.magnitude > maxSpeed)
         {
@@ -165,6 +193,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Crouch()
     {
-
+        if (isCrouching)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
     }
 }
